e87b4528f0aab7faa48a93473f67c48ad07feabe
zaid ragab <zragab@lcs.on.ca>
2023-01-12 11:46:08 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by Zaid Ragab on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
